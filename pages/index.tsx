adcfdb598ed5569d159deb6e1ce6cd503f0d43cb
divinevibration <83971946+divinevibration@users.noreply.github.com>
2022-01-07 15:03:53 +0200

    updated index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to{" "}
-          <a href="https://nextjs.org">Next.js, TypeScript, Tailwindcss!</a>
+          <a href="https://nextjs.org">Next.js, TypeScript & Tailwindcss!</a>
         </h1>
         <p className={styles.description}>
           Get started by editing the Template
